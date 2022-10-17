1b134576111bb24f044bb09b3398f88ecd7c8772
Johanna-A-02 <leidy.almonacid.mt@usa.edu.co>
2022-10-17 00:24:36 -0500

    test commented

--- src/test/java/com/reto/RetoApplicationTests.java
+++ src/test/java/com/reto/RetoApplicationTests.java
@@ -3,11 +3,11 @@ package com.reto;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class RetoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
